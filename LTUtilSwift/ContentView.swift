7e5794699ac1671f859164d2ad03170116350e91
LTOVE <ltovem@163.com>
2019-06-19 15:57:23 +0800

    init

---
+++ LTUtilSwift/ContentView.swift
@@ -0,0 +1,29 @@
+//
+// ContentView.swift
+// LTUtilSwift
+//
+//  Auther:    田高伟
+//  email:     mailto:t@ltove.com
+//  webSite:   https://www.ltove.com
+//  GitHub:    https://github.com/LTOVEM/
+//
+// Created by LTOVE on 2019/6/19.
+// Copyright © 2019 LTOVE. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
